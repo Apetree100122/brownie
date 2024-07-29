f9535a352927ca09f25a0a4a369d72a24412ead7
Alexander Petree <apetree1001@email.phoenix.edu>
2024-07-29 00:32:20 -0700

    Update typing.py

--- brownie/typing.py
+++ brownie/typing.py
@@ -1,11 +1,7 @@
-from typing import TypeVar
-
-from brownie.network.account import Accounts
-from brownie.network.transaction import TransactionReceipt
-
-# NETWORK
-# Account
-AccountsType = TypeVar("AccountsType", bound=Accounts)
-
-# Transactions
-TransactionReceiptType = TypeVar("TransactionReceiptType", bound=TransactionReceipt)
+from typing import TypeVar from 
+brownie.network.account import Accounts from brownie.network.transaction 
+import TransactionReceipt
+# NETWORK # Account
+AccountsType = TypeVar("AccountsType", bound=Accounts
+# Transactions TransactionReceiptType = TypeVar
+("TransactionReceiptType", bound=TransactionReceipt)
